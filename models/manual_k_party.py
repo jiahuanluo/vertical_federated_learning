3f89accd1a694a6e1223e3a1179a9100527754d2
jiahaunluo <luojiahuan001@gmail.com>
2020-11-10 22:09:31 +0800

    add loss

--- models/manual_k_party.py
+++ models/manual_k_party.py
@@ -8,7 +8,6 @@ class Manual_A(nn.Module):
         super(Manual_A, self).__init__()
         self.net = models.resnet18(pretrained=False, num_classes=u_dim)
         self.classifier = nn.Linear(u_dim, 40)
-        self.contrastive_loss = 0
         self.T = 0.07
         self.register_buffer("queue", torch.randn(64, 1024))
         self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
@@ -25,38 +24,42 @@ class Manual_A(nn.Module):
 
         self.queue_ptr[0] = ptr
 
-    def forward(self, input, U_B):
+    def forward(self, input):
         out = self.net(input)
-        if U_B is not None:
-            if not torch.sum(U_B[0]) == 0:
-                l_pos = torch.einsum('nc,nc->n', [out, U_B[0]]).unsqueeze(-1)
-                l_neg = torch.einsum('nc,ck->nk', [out, self.queue.clone().detach()])
-                # apply temperature
-                logits = torch.cat([l_pos, l_neg], dim=1)
-                logits /= self.T
+        return out
 
-                # labels: positive key indicators
-                labels = torch.zeros(logits.shape[0], dtype=torch.long).cuda()
+    def contrastive_loss(self, U_A, U_B):
+        if not torch.sum(U_B) == 0:
+            l_pos = torch.einsum('nc,nc->n', [U_A, U_B[0]]).unsqueeze(-1)
+            l_neg = torch.einsum('nc,ck->nk', [out, self.queue.clone().detach()])
+            # apply temperature
+            logits = torch.cat([l_pos, l_neg], dim=1)
+            logits /= self.T
+            # labels: positive key indicators
+            labels = torch.zeros(logits.shape[0], dtype=torch.long).cuda()
+            contrastive_loss = nn.CrossEntropyLoss().cuda()(logits, labels)
+            self._dequeue_and_enqueue(U_B)
+            return contrastive_loss
+        else:
+            return 0
 
-                self.contrastive_loss = nn.CrossEntropyLoss().cuda()(logits, labels)
-                self._dequeue_and_enqueue(U_B[0])
-                # self.contrastive_loss = torch.sum((out - U_B[0])**2) / out.size(0)
-                # self.contrastive_loss = torch.abs(torch.sum(out * U_B[0]) / out.size(0))
-                # if torch.sum(U_B[0]) == 0:
-                #     out = out
-                # else:
-                #     out = out * U_B[0]
-                # out = out + U_B[0]
-                # out = torch.cat([out] + [U for U in U_B], dim=1)
-        logits = self.classifier(out)
-        return logits, self.contrastive_loss
+    def dist_loss(self, U_A, U_B, dist_type='eu'):
+        if not torch.sum(U_B) == 0:
+            if dist_type == 'eu':
+                dist_loss = torch.sum((U_A - U_B) ** 2) / U_A.size(0)
+            else:
+                dist_loss = torch.abs(torch.sum(U_A * U_B) / out.size(0))
+            return dist_loss
+        else:
+            return 0
 
-    def contrastive_loss(self):
-        raise NotImplementedError
+    def classify(self, U_A, U_B):
+        U = U_A + U_B
+        out = self.classifier(U)
+        return out
 
 
 class Manual_B(nn.Module):
-
     def __init__(self, u_dim=64):
         super(Manual_B, self).__init__()
         self.net = models.resnet18(pretrained=False, num_classes=u_dim)
